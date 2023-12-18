45b2e6522f6bab5960b4a01c1afaf8eec04b4239
Hc-16 <hc16@email.com>
2023-12-18 23:31:16 +0100

    media

--- QueComemosHoy/settings.py
+++ QueComemosHoy/settings.py
@@ -132,8 +132,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-MEDIA_URL = 'media/'
-MEDIA_ROOT = BASE_DIR/ 'media'
+
+MEDIA_URL = os.environ.get('MEDIA_URL', 'media/')
+MEDIA_ROOT = os.environ.get('MEDIA_ROOT', (BASE_DIR/ 'media'))
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
